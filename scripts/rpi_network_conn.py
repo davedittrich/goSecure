0b3e5a46e8d7554f127f3fa65696d72fa96ab31e
Dave Dittrich <dittrich@u.washington.edu>
2020-03-08 18:07:22 -0700

    Fix bug when wlan interface is not up

--- scripts/rpi_network_conn.py
+++ scripts/rpi_network_conn.py
@@ -22,6 +22,7 @@ def get_wifi_list():
         wlan_status = get_output(["sudo", "ifup", "wlan0"])
         returncode = 0
     except CalledProcessError as e:
+        wlan_status = []
         returncode = e.returncode
     for line in wlan_status:
         logger.debug(line)
